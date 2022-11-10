d6e30c1857bacc8ac8afd4c3f63406f8d0908c04
Keldrion <2306260+keldrion@users.noreply.github.com>
2022-11-10 12:34:42 +0100

    Add files via upload

--- start.cmd
+++ start.cmd
@@ -3,8 +3,7 @@ mode con lines=2 cols=29
 title   
 set "ROOT=%CD%"
 set "TOOLS=%ROOT%\tools"
-if exist "%TOOLS%\*.7z" ("%TOOLS%\7z" x -o"%TOOLS%\" "%TOOLS%\*.7z" >nul 2>&1 && del "%TOOLS%\*.7z" >nul 2>&1 && "move %TOOLS%\7z* %TOOLS%\bin" >nul)
-if exist "%TOOLS%\bin\java\lib\*.7z" ("%TOOLS%\7z" x "-o%TOOLS%\bin\java\lib\" "%TOOLS%\bin\java\lib\*.7z" >nul 2>&1 && del "%TOOLS%\bin\java\lib\*.7z" >nul 2>&1)  
+if exist "%TOOLS%\*.7z" ("%TOOLS%\7z" x -o"%TOOLS%" "%TOOLS%\*.7z" >nul 2>&1) && move "%TOOLS%\7z*" "%TOOLS%\bin" >nul && if exist "%TOOLS%\bin\java\lib\*.7z" ("%TOOLS%\bin\7z" x -o"%TOOLS%\bin\java\lib" "%TOOLS%\bin\java\lib\*.7z"  >nul 2>&1 && del "%TOOLS%\*.7z" "%TOOLS%\bin\java\lib\*.7z")
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' NEQ '0' (echo  ...Getting Admin Rights...  && timeout 2 >nul && goto UAC) else (goto gotAdmin)
 :UAC
