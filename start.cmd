da5e9b387bb6d035d8d38b6ac8a0637529bd58c7
Keldrion <2306260+keldrion@users.noreply.github.com>
2022-11-10 02:40:44 +0100

    Add files via upload

---
+++ start.cmd
@@ -0,0 +1,16 @@
+@echo off & color 0C
+mode con lines=2 cols=29
+title   
+set "ROOT=%CD%"
+set "BIN=%ROOT%\tools\bin"
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (echo  ...Getting Admin Rights...  && timeout 2 >nul && goto UAC) else (goto gotAdmin)
+:UAC
+echo Set UAC = CreateObject^("Shell.Application"^) > "%BIN%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%BIN%\getadmin.vbs"
+"%BIN%\getadmin.vbs" && del %BIN%\getadmin.vbs
+exit /B
+:gotAdmin
+pushd "%CD%"
+CD /D "%~dp0.\tools"
+.\1
